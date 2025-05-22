d3d0f28268ea28681f029f86296353005a8a7785
Jae Hee Kim <jhk37@sfu.ca>
2025-05-22 15:17:14 -0700

    lab1.c submission

--- lab1.c
+++ lab1.c
@@ -1,1 +1,45 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(void) {
+  // getline() allows you to read a full line of text from the keyboard:
+  // tokenize using strtok_r()
+
+  char *line = NULL;
+  size_t n = 0;
+
+  printf("Please enter some text: \n");
+
+  if (getline(&line, &n, stdin) != -1) {
+    printf("Tokens:\n");
+    char *ret = NULL;
+    char *saveptr = NULL;
+    char *str = line;
+    while ((ret = strtok_r(str, " ", &saveptr))) {
+      printf("%s\n", ret);
+      str = NULL;
+    }
+    free(line);
+  } else {
+    printf("Failure reading line!\n");
+  }
+
+  return 0;
+
+  /*ssize_t num_char = getline(&buff, &size, stdin);
+  if (num_char == -1) {
+    perror("getline failed");
+    exit(EXIT_FAILURE);
+  }
+
+  char *saveptr;
+  char *ret = strtok_r(buff, " ", &saveptr);
+
+
+  char *ret2 = strtok_r(NULL, " ", &saveptr);
+
+  free(buff);
+  */
+}
