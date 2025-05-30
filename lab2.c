e542b595e17da05257682c8df28efab2045ab5ac
Jae Hee Kim <jhk37@sfu.ca>
2025-05-30 01:13:29 -0700

    lab2.c submission

---
+++ lab2.c
@@ -0,0 +1,46 @@
+// Lab 2 - fork(), exec(), waitpid()
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
+
+int main(void) {
+  char *line = NULL;
+  size_t n;
+
+  while (1) {
+    printf("Enter programs to run.\n");
+    ssize_t len = getline(&line, &n, stdin);
+    if (len == -1) {
+      perror("Failed to read line!\n");
+      break;
+    } else if (len > 0) {
+      line[len - 1] = '\0';
+    }
+
+    pid_t pid = fork();
+
+    if (pid) {
+      int status = 0;
+
+      if (waitpid(pid, &status, 0) == -1) {
+        perror("waitpid failed.\n");
+        exit(EXIT_FAILURE);
+      }
+
+      if (WIFEXITED(status)) {
+        printf("Child exited.\n");
+      }
+      free(line);
+      line = NULL;
+
+    } else {
+      if (execl(line, line, (char *)NULL) == -1) {
+        perror("exec failed.\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+  return 0;
+}
